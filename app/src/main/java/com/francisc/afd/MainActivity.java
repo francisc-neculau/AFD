56a57973be4d8edef1922caf29b5e36b5049b2c0
francisc-neculau <francisc.neculau@gmail.com>
2017-01-06 22:33:10 +0200

    Adding Toolbar

--- app/src/main/java/com/francisc/afd/MainActivity.java
+++ app/src/main/java/com/francisc/afd/MainActivity.java
@@ -1,22 +1,24 @@
 package com.francisc.afd;
 
 import android.os.PersistableBundle;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity
 {
-    /*
-    Toolbar
-    RecyclerView
-    Menu
-     */
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setTitle("AFD");
+        toolbar.setSubtitle("welcome");
     }
 
     @Override
